e9d252727a8cdf6b965071bbde30c957c38a3c1b
quangsang044 <ngoquangsang.44@gmail.com>
2023-01-13 13:48:25 +0700

    Create Cau 1.sql

---
+++ Cau 1.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO department(DepartID, DepartName, Descriptions)
+VALUES (1, "HN", "Ha Noi"),
+       (2, "HN", "Ha Noi"),
+       (3, "HP", "Ha Noi");
+
+INSERT INTO employee(EmpCode, FirstName, LastName, Birthday, Gender, Address, DepartID, Salary)
+VALUES ("01", "Nguyen", "An", "2002-08-02", 1, "Ha Noi", "1", 250000),
+       ("02", "Nguyen", "Binh", "2002-08-02", 2, "Ha Noi", "1", 300000),
+       ("03", "Nguyen", "Cuong", "2002-08-02", 3, "Ha Noi", "1", 400000);
